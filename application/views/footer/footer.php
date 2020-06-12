74d07d4ef565bb1c75a6816ae5202a33f72f7f08
lakshitha1629 <lakshitha1629@gmail.com>
2020-06-12 23:44:15 +0530

    update ui

--- application/views/footer/footer.php
+++ application/views/footer/footer.php
@@ -1,9 +1,11 @@
+
 <!-- Footer -->
-<footer id="footer" class=""><!-- section -->
+<footer id="footer" class="">
+    <!-- section -->
 
     <!-- container -->
     <div class="container">
-        
+
         <!-- row -->
         <div id="bottom-footer" class="row">
 
@@ -40,3 +42,21 @@
 
 </footer>
 <!-- /Footer -->
+
+<!-- Modal Start -->
+<?php if (isset($modal)) echo $modal; ?>
+<footer id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <p class="text-muted" style="margin: 10px 0;">U0NSSVBUIFNIQVJFRCBPTiBDT0RFTElTVC5DQw==</p>
+            </div>
+        </div>
+    </div>
+</footer><!--/#footer-->
+
+<div id="fade" class="black_overlay"></div> 
+<!-- Common Scripts-->
+<script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js') ?>"></script>
+<!-- Custom JS  -->
+<script src="<?php echo base_url('assets/js/jsscript.js') ?>"></script>
--- application/views/header/head.php
+++ application/views/header/head.php
@@ -12,9 +12,8 @@
 <!--[if lt IE 9]>
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
-<!-- <script src="<?php echo base_url('assets/js/jquery-2.0.3.min.js') ?>"></script> -->
+<script src="<?php echo base_url('assets/js/jquery-2.0.3.min.js') ?>"></script>
 <!-- <script src="<?php echo base_url('assets/js/video.js') ?>"></script> -->
-
 
 
 <!-- new -->
@@ -29,9 +28,3 @@
 <!-- <script type="text/javascript" src="<?php echo base_url('assets/new/js/jquery.min.js') ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/new/js/bootstrap.min.js') ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/new/js/main.js') ?>"></script> -->
-
-
-<!-- Common Scripts-->
-<script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js') ?>"></script>
-<!-- Custom JS  -->
-<script src="<?php echo base_url('assets/js/jsscript.js') ?>"></script>
